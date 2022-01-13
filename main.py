293686507e0819f4168e7031c0241ccbbdada272
mahendrakumar <mahendran638161@gmail.com>
2022-01-13 14:50:38 +0530

    made changes in files

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from flask import Flask, render_template, request
 from flask.typing import StatusCode
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user = "root",
+    password ="kalisee",
+    database="feedback"
+)
+cursor = mydb.cursor()
 
 app = Flask(__name__)
+
+class User():
+    userArr = []
+    def __init__(self,name,staff_name,rating,detail) -> None:
+        self.name = name
+        self.staff_name = staff_name
+        self.rating = rating
+        self.detail = detail
+        
+
+    def insertion(self):
+        query = f"insert into user_feedback (name,staff_name,rating,detail) values ( {self.name} ,{self.staff_name} ,{self.rating} ,{self.detail})"
+
+    @classmethod
+    def deletion(cls)    
+
+        
 
 
 @app.route('/')
